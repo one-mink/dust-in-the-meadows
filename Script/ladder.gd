fb356b6ba15fe63fd98a89a7b5c50cc108c1dd04
ysxaybarroso-ux <ysxaybarroso@gmail.com>
2026-09-21 16:16:21 +0200

    i add the start of the first level only prototype to test features like ladder and doors

--- Script/ladder.gd
+++ Script/ladder.gd
@@ -4,7 +4,7 @@ var is_on_zone = false
 var player = null
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_on_zone and Input.is_action_pressed("Jump"):
 		player.velocity.y = -200
 		
