85839341863003a6c48521461ee1f1fac30fea42
ysxaybarroso-ux <ysxaybarroso@gmail.com>
2026-09-21 23:30:10 +0200

    add the house and change first level

--- Script/player.gd
+++ Script/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name Player
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -550.0
+const JUMP_VELOCITY = -300.0
 
 signal item_collected(item)
 @onready var player: Player = $"."
